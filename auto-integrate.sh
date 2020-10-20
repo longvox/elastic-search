a04213921116d8ebc9801a8681fe74821f20124d
imlong <longvox98@gmail.com>
2020-10-20 11:45:09 +0700

    fix: update log auto integrate

--- auto-integrate.sh
+++ auto-integrate.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
+echo '[+] - Copy file in folder conf.d to /etc/logstash/conf.d/'
 sudo cp conf.d/* /etc/logstash/conf.d/
+
+echo '[+] - Copy file pipline.yml to /etc/logstash/'
 sudo cp pipelines.yml /etc/logstash/
+
+echo '[+] - Copy file logstash.yml to /etc/logstash/'
 sudo cp logstash.yml /etc/logstash/
 
-if [ $1 = 'restart' ]
+if [ "$#" -eq  "0" ]
+then
+  echo '[DONE]'
+elif [ $1 = 'restart' ]
 then
   source ./auto-retart-logstash.sh
 fi
