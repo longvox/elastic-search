2678b017f91db261fce4f1ccd2d9682324757eaf
imlong <longvox98@gmail.com>
2020-10-20 10:24:28 +0700

    update restart logstash

--- auto-integrate.sh
+++ auto-integrate.sh
@@ -1,2 +1,3 @@
 sudo cp conf.d/* /etc/logstash/conf.d/
 sudo cp pipelines.yml /etc/logstash/
+sudo cp logstash.yml /etc/logstash/
--- auto-retart-logstash.sh
+++ auto-retart-logstash.sh
@@ -1,3 +1,9 @@
-sudo kill -TERM $(ps -ef | grep logstash | awk '{print $2}' )
+echo '[+] - Kill all logstash'
+sudo ps -ef | sudo grep logstash | sudo awk '{kill -9 $2}'
+
+echo '[+] - Stop logstash'
 sudo service logstash stop
+
+echo '[+] - Start logstash'
 sudo service logstash start
+echo '[DONE]'
